d6b3ba28b15786a32ee68c5ca5d09125cfbea893
Aneel Raju <aneelraju@gmail.com>
2018-03-04 11:24:49 +0530

    add changes to 03 variables and gotraining

---
+++ work/03_variables/01_shorthand/01/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// var g string; // declare
+// var g string = "cowboy"; // initialize
+// g = "cowboy"; // assign
+
+func main() {
+
+	a := 10 // shorthand assign
+	b := "golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%v \n", a)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+}
---
+++ work/03_variables/01_shorthand/02/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 10 // shorthand assign
+	b := "golang"
+	c := 4.17
+	d := true
+
+	fmt.Printf("%T \n", a) // type of the variable
+	fmt.Printf("%T \n", b)
+	fmt.Printf("%T \n", c)
+	fmt.Printf("%T \n", d)
+}
