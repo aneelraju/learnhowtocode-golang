6838dc12fdf451df86248944f7232d164547cbc2
Aneel Raju <aneelraju@gmail.com>
2018-03-11 09:32:10 +0530

    added changes to work/10_for_loop

---
+++ work/10_for-loop/03_for-condition-while-ish/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 0
+	// similar to while loop
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
+}
---
+++ work/10_for-loop/07_rune-loop_UTF8/01/hello_bytes.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	// prints byte of letters H e l l o
+	fmt.Println([]byte("Hello"))
+}
